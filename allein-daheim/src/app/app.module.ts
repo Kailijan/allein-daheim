d2dac4f57c1c413b7046c0854578e9e6b8755e0c
Mario Geier <marioegbertgeier@gmail.com>
2020-03-21 12:03:47 +0100

    Import textchat page in module

--- allein-daheim/src/app/app.module.ts
+++ allein-daheim/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ApiTopicMatchingProvider } from '../providers/api-topic-matching/api-topic-matching';
+import { TextchatPage } from '../pages/textchat/textchat';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    TextchatPage
   ],
   imports: [
     BrowserModule,
@@ -24,7 +26,8 @@ import { ApiTopicMatchingProvider } from '../providers/api-topic-matching/api-to
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    TextchatPage
   ],
   providers: [
     StatusBar,
