3d6f26df1f0f2bb0cba0d500f582988375ed5611
Mario Geier <marioegbertgeier@gmail.com>
2020-03-21 15:52:51 +0100

    Move code to chatstorage and use interfaces

--- allein-daheim/src/pages/textchat/textchat.ts
+++ allein-daheim/src/pages/textchat/textchat.ts
@@ -1,5 +1,8 @@
-import { Component, ViewChild, trigger, state, style, transition, animate, AfterViewChecked } from '@angular/core';
+import { Component, ViewChild, trigger, state, style, transition, animate, AfterViewChecked, Input } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ChatStorageProvider } from '../../providers/chat-storage/chat-storage';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { Observable } from 'rxjs/Observable';
 import { TextMessage } from './text-message/text-message';
 
 /**
@@ -32,30 +35,16 @@ export class TextchatPage implements AfterViewChecked {
   @ViewChild('messageTextBox') messageTextBox;
   @ViewChild('messageListElement') messageListElement;
 
+  @Input() receiverId;
+  @Input() senderId;
+
+  public $messsages: Observable<Array<TextMessage>>;
+
   public receiverName = 'Peter';
-  public receiverId = 20;
-  public messages: Array<TextMessage>;
 
-  getRandomInt(max): number {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.messages = new Array<TextMessage>();
-    for (var i = 0; i < 8; i++) {
-      const newMessage = {
-        content:  Math.random().toString(36),
-        sent:     new Date(),
-        sender:   0,
-        receiver: this.getRandomInt(2) == 1 ? this.receiverId : 0};
-      this.messages.push(newMessage)
-    }
-    const newMessage = {
-      content:  "alksdjflkasdlkfakjsdfkjaskdjfklasdjkfkjasdjkfhsakjdfhasdkjfhaskdjfhaksjldfhakjsdhfkjasdhf asdkjfhaksjdhfkajshdf kjasdhfkjahsdfkjhasdkjfhaksjdfhaksjdhfkjasdhkfjashdfkjashdfkjasdhfkjasdhfkjasdhfkjashdfkjahsdkfjhasdkfjhasdkjfhaksjkdfhaskdjfhaksjdfh",
-      sent:     new Date(),
-      sender:   0,
-      receiver: this.getRandomInt(2) == 1 ? this.receiverId : 0};
-    this.messages.push(newMessage)
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private chatStorage: ChatStorageProvider) {
+    this.$messsages = this.chatStorage.getMessages(this.receiverId);
   }
 
   ionViewDidLoad() {
@@ -79,9 +68,9 @@ export class TextchatPage implements AfterViewChecked {
 
     const newMessage = { content:  this.messageTextBox.value,
                          sent:     new Date(),
-                         sender:   0,
+                         sender:   this.senderId,
                          receiver: this.receiverId };
-    this.messages.push(newMessage);
+    this.chatStorage.addMessage(newMessage);
     this.messageTextBox.value = '';
   }
 
--- allein-daheim/src/providers/chat-storage/chat-storage.ts
+++ allein-daheim/src/providers/chat-storage/chat-storage.ts
@@ -1,17 +1,47 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { TextMessage } from '../../pages/textchat/text-message/text-message';
+import { Observable } from 'rxjs/Observable';
+import { ReplaySubject } from 'rxjs';
 
-/*
-  Generated class for the ChatStorageProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ChatStorageProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello ChatStorageProvider Provider');
+  private messageArray = new Array<TextMessage>();
+  public $messages = new ReplaySubject<Array<TextMessage>>(1);
+  public receiverId = 20;
+
+  getRandomInt(max): number {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  constructor() {
+    for (var i = 0; i < 8; i++) {
+      const newMessage = {
+        content:  Math.random().toString(36),
+        sent:     new Date(),
+        sender:   0,
+        receiver: this.getRandomInt(2) == 1 ? this.receiverId : 0};
+      this.addMessage(newMessage)
+    }
+    const newMessage = {
+      content:  "alksdjflkasdlkfakjsdfkjaskdjfklasdjkfkjasdjkfhsakjdfhasdkjfhaskdjfhaksjldfhakjsdhfkjasdhf asdkjfhaksjdhfkajshdf kjasdhfkjahsdfkjhasdkjfhaksjdfhaksjdhfkjasdhkfjashdfkjashdfkjasdhfkjasdhfkjasdhfkjashdfkjahsdkfjhasdkfjhasdkjfhaksjkdfhaskdjfhaksjdfh",
+      sent:     new Date(),
+      sender:   0,
+      receiver: this.getRandomInt(2) == 1 ? this.receiverId : 0};
+    this.addMessage(newMessage)
+  }
+
+  public addMessage(message: TextMessage) {
+    this.messageArray.push(message);
+    this.$messages.next(this.messageArray);
+  }
+
+  public getMessages(receiverId: number): Observable<Array<TextMessage>> {
+    return this.$messages.map(
+      (list) => list.filter((message) => {
+        return message.receiver == receiverId || message.sender == receiverId;
+    }));
   }
 
 }
